7fc47a8974b94e1ad7efb194063ea8ebb194f865
Richard Spindler <richard@lateralblast.com.au>
2014-06-03 09:32:12 +1000

    Documentation update and initial commit to github

---
+++ README.md
@@ -0,0 +1,224 @@
+![alt tag](https://raw.githubusercontent.com/lateralblast/eclair/master/eclair.jpg)
+
+ECLAIR
+======
+
+ESX Command Line Automation In Ruby
+
+Introduction
+------------
+
+Eclair is a ruby script that automates various aspects for ESXi configuration
+and updating. The automation is done via SSH. This is done with the ruby net/ssh
+and net/scp gems. The goal was to make a command line tool to do this so that I
+could automate the process across a number of machines.
+
+Eclair also has the capability of getting the latest patches from the WMware
+website. Due to the use of Javascript on the VMware download site this had to
+be achieved with a combination of the selenium-webdriver, phantomjs and nokogiri gems.
+
+Features:
+
+- Upgrade/Downgrade ESXi
+  - From local repository or from VMware
+    - When using local reposity it copies update to /scratch/downloads on ESXi host and then installs
+    - The local repository by default sits in a "patches" directory in the same directory as the script
+- Get a list of available patches for a release of ESXi from the VMware web site
+- Download any patches from the VMware site that are not in the local repository
+- The ESXi username and password can be stored in a local file so they don't appear on the command line
+
+Requirements:
+
+- ruby
+- wget
+- ruby gems
+  - net/ssh
+  - net/scp
+  - etc
+  - expect
+  - getopt/std
+  - selenium-webdriver
+  - phantomjs
+  - nokogiri
+
+Issues:
+
+- The URLs given by the VMware site are dynamic session based URLs, so will expire soon after the are created
+  - If you are going to download them manually, you'll need to do it soon after you run the script
+
+Todo:
+
+- Setup an ESXi host
+  - NTP
+  - Syslog
+  - SNMP
+- Lockdown ESXi host
+
+
+Usage
+-----
+
+```
+$ eclair.rb -[AbCDf:hl:LP:r:Rs:Sp:u:UVyZ]
+
+-h:     Print usage information
+-V:     Print version information
+-U:     Update ESX if newer patch level is available
+-Z:     Downgrade ESX to earlier release
+-L:     List all available versions in local patch directory
+-R:     List all available versions in VMware depot
+-A:     Download available patches to local patch directory
+-C:     Check if newer patch level is available
+-r:     Upgrade or downgrade to a specific release
+-s:     Hostname
+-p:     Password
+-f:     Source file for update
+-D:     Patch directory (default is patches in sames directory as script)
+-y:     Perform action (if not given you will be prompted before upgrades)
+-S:     Setup ESXi (Syslog, NTP, etc)
+-l:     Check if a particular patch is in the local repository
+-b:     Perform reboot after patch installation (not default)
+```
+
+Examples
+--------
+
+Check update installed on ESX host against what is available on VMware site:
+
+```
+$ eclair.rb -C -s 192.168.1.183
+Current:   20140404001
+Available: 20140404001
+Local patch level is up to date
+```
+
+Check update installed on ESX host against a patch ID in the local repository:
+
+```
+$ eclair.rb -C -s 192.168.1.183 -f ESXi550-201404020
+File:  /Users/spindler/Code/eclair/patches/ESXi550-201404020.zip
+Current:   20140302001
+Available: 20140401020s
+Depot patch level is newer than installed version
+```
+
+Check update installed on ESX host against a patch file in the local repository:
+
+```
+$ eclair.rb -C -s 192.168.1.183 -f ./patches/ESXi550-201404020.zip
+File:  /Users/spindler/Code/eclair/patches/ESXi550-201404020.zip
+Current:   20140302001
+Available: 20140401020s
+Depot patch level is newer than installed version
+```
+
+Updating an ESXi host from a patch in the local repository (without confirmation and reboot):
+
+```
+$ eclair.rb -U -s 192.168.1.183 -f ESXi550-201404001 -y -b
+File:  /Users/spindler/Code/eclair/patches/ESXi550-201404001.zip
+Current:   20140302001
+Available: 20140404001
+Depot patch level is newer than installed version
+Copying local file /Users/spindler/Code/eclair/patches/ESXi550-201404001.zip to 192.168.1.183:/scratch/downloads/ESXi550-201404001.zip
+Installing ESXi-5.5.0-20140404001-standard from /scratch/downloads/ESXi550-201404001.zip
+Update Result
+   Message: The update completed successfully, but the system needs to be rebooted for the changes to be effective.
+   Reboot Required: true
+   VIBs Installed: VMware_bootbank_esx-base_5.5.0-1.16.1746018, VMware_bootbank_lsi-mr3_0.255.03.01-2vmw.550.1.16.1746018, VMware_locker_tools-light_5.5.0-1.16.1746018
+   VIBs Removed: VMware_bootbank_esx-base_5.5.0-1.15.1623387, VMware_bootbank_lsi-mr3_0.255.03.01-1vmw.550.0.0.1331820, VMware_locker_tools-light_5.5.0-1.15.1623387
+   VIBs Skipped: VMware_bootbank_ata-pata-amd_0.3.10-3vmw.550.0.0.1331820, VMware_bootbank_ata-pata-atiixp_0.4.6-4vmw.550.0.0.1331820,
+   VMware_bootbank_ata-pata-cmd64x_0.2.5-3vmw.550.0.0.1331820, VMware_bootbank_ata-pata-hpt3x2n_0.3.4-3vmw.550.0.0.1331820,
+   VMware_bootbank_ata-pata-pdc2027x_1.0-3vmw.550.0.0.1331820, VMware_bootbank_ata-pata-serverworks_0.4.3-3vmw.550.0.0.1331820,
+   VMware_bootbank_ata-pata-sil680_0.4.8-3vmw.550.0.0.1331820, VMware_bootbank_ata-pata-via_0.3.3-2vmw.550.0.0.1331820,
+   VMware_bootbank_block-cciss_3.6.14-10vmw.550.0.0.1331820, VMware_bootbank_ehci-ehci-hcd_1.0-3vmw.550.0.0.1331820,
+   VMware_bootbank_elxnet_10.0.100.0v-1vmw.550.0.0.1331820, VMware_bootbank_esx-dvfilter-generic-fastpath_5.5.0-0.0.1331820,
+   VMware_bootbank_esx-tboot_5.5.0-0.0.1331820, VMware_bootbank_esx-xlibs_5.5.0-0.0.1331820, VMware_bootbank_esx-xserver_5.5.0-0.0.1331820,
+   VMware_bootbank_ima-qla4xxx_2.01.31-1vmw.550.0.0.1331820, VMware_bootbank_ipmi-ipmi-devintf_39.1-4vmw.550.0.0.1331820,
+   VMware_bootbank_ipmi-ipmi-msghandler_39.1-4vmw.550.0.0.1331820, VMware_bootbank_ipmi-ipmi-si-drv_39.1-4vmw.550.0.0.1331820,
+   VMware_bootbank_lpfc_10.0.100.1-1vmw.550.0.0.1331820, VMware_bootbank_lsi-msgpt3_00.255.03.03-1vmw.550.1.15.1623387,
+   VMware_bootbank_misc-cnic-register_1.72.1.v50.1i-1vmw.550.0.0.1331820, VMware_bootbank_misc-drivers_5.5.0-0.7.1474526,
+   VMware_bootbank_mtip32xx-native_3.3.4-1vmw.550.1.15.1623387, VMware_bootbank_net-be2net_4.6.100.0v-1vmw.550.0.0.1331820,
+   VMware_bootbank_net-bnx2_2.2.3d.v55.2-1vmw.550.0.0.1331820, VMware_bootbank_net-bnx2x_1.72.56.v55.2-1vmw.550.0.0.1331820,
+   VMware_bootbank_net-cnic_1.72.52.v55.1-1vmw.550.0.0.1331820, VMware_bootbank_net-e1000_8.0.3.1-3vmw.550.0.0.1331820,
+   VMware_bootbank_net-e1000e_1.1.2-4vmw.550.1.15.1623387, VMware_bootbank_net-enic_1.4.2.15a-1vmw.550.0.0.1331820,
+   VMware_bootbank_net-forcedeth_0.61-2vmw.550.0.0.1331820, VMware_bootbank_net-igb_5.0.5.1.1-1vmw.550.1.15.1623387,
+   VMware_bootbank_net-ixgbe_3.7.13.7.14iov-11vmw.550.0.0.1331820, VMware_bootbank_net-mlx4-core_1.9.7.0-1vmw.550.0.0.1331820,
+   VMware_bootbank_net-mlx4-en_1.9.7.0-1vmw.550.0.0.1331820, VMware_bootbank_net-nx-nic_5.0.621-1vmw.550.0.0.1331820,
+   VMware_bootbank_net-tg3_3.123c.v55.5-1vmw.550.1.15.1623387, VMware_bootbank_net-vmxnet3_1.1.3.0-3vmw.550.0.0.1331820,
+   VMware_bootbank_ohci-usb-ohci_1.0-3vmw.550.0.0.1331820, VMware_bootbank_qlnativefc_1.0.12.0-1vmw.550.0.0.1331820,
+   VMware_bootbank_rste_2.0.2.0088-4vmw.550.1.15.1623387, VMware_bootbank_sata-ahci_3.0-18vmw.550.1.15.1623387,
+   VMware_bootbank_sata-ata-piix_2.12-9vmw.550.0.0.1331820, VMware_bootbank_sata-sata-nv_3.5-4vmw.550.0.0.1331820,
+   VMware_bootbank_sata-sata-promise_2.12-3vmw.550.0.0.1331820, VMware_bootbank_sata-sata-sil24_1.1-1vmw.550.0.0.1331820,
+   VMware_bootbank_sata-sata-sil_2.3-4vmw.550.0.0.1331820, VMware_bootbank_sata-sata-svw_2.3-3vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-aacraid_1.1.5.1-9vmw.550.0.0.1331820, VMware_bootbank_scsi-adp94xx_1.0.8.12-6vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-aic79xx_3.1-5vmw.550.0.0.1331820, VMware_bootbank_scsi-bnx2fc_1.72.53.v55.1-1vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-bnx2i_2.72.11.v55.4-1vmw.550.0.0.1331820, VMware_bootbank_scsi-fnic_1.5.0.4-1vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-hpsa_5.5.0-44vmw.550.0.0.1331820, VMware_bootbank_scsi-ips_7.12.05-4vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-lpfc820_8.2.3.1-129vmw.550.0.0.1331820, VMware_bootbank_scsi-megaraid-mbox_2.20.5.1-6vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-megaraid-sas_5.34-9vmw.550.1.15.1623387, VMware_bootbank_scsi-megaraid2_2.00.4-9vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-mpt2sas_14.00.00.00-3vmw.550.1.15.1623387, VMware_bootbank_scsi-mptsas_4.23.01.00-9vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-mptspi_4.23.01.00-9vmw.550.0.0.1331820, VMware_bootbank_scsi-qla2xxx_902.k1.1-9vmw.550.0.0.1331820,
+   VMware_bootbank_scsi-qla4xxx_5.01.03.2-6vmw.550.0.0.1331820, VMware_bootbank_uhci-usb-uhci_1.0-3vmw.550.0.0.1331820
+Rebooting
+```
+
+Get a list of the available patches for ESXi for 5.1.0:
+
+```
+$ eclair.rb -R -r 5.1.0
+Update:   ESXi510-201404001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-431-20140427-641697/ESXi510-201404001.zip?HashKey=b67d1404781d0d791f4f911065073499&AuthKey=1401676093_4dc39ad8083dbde1c34e6e526a5
+45d5e
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201404001.zip
+Update:   ESXi510-201402001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-420-20140226-922881/ESXi510-201402001.zip?HashKey=67c081d74083f2c3a10f94000b43d2fd&AuthKey=1401676093_a33f4b25ac645443bcf2926c7f7
+7f17b
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201402001.zip
+Update:   ESXi510-201310001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-402-20131016-227919/ESXi510-201310001.zip?HashKey=ba0c39f3a9ea054afbe3f5feb6d1d56d&AuthKey=1401676093_67e756d758478965261a19ca5dc
+067bf
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201310001.zip
+Update:   ESXi510-201307001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-394-20130722-233368/ESXi510-201307001.zip?HashKey=df48dcad4875176724df7f8f850e7061&AuthKey=1401676093_5c0379a805748318d618ad827fe
+97268
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201307001.zip
+Update:   ESXi510-201305001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-387-20130519-806235/ESXi510-201305001.zip?HashKey=15a0c1c2a4b929d0bf421c4d9705158f&AuthKey=1401676093_e2a0c5220bf9cb6c8644fd74d59
+0c9c4
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201305001.zip
+Update:   ESXi510-201303001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-375-20130304-043403/ESXi510-201303001.zip?HashKey=87509815d8ae55ff4a8051707bbf2eaa&AuthKey=1401676093_645a458051fb743ab3b0ea2cde6
+4cc5d
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201303001.zip
+Update:   ESXi510-201212001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-368-20121217-718319/ESXi510-201212001.zip?HashKey=d56cbde35ce7e3a5deb6932e4d878505&AuthKey=1401676093_88e26c9013bc8f8d205409097f9
+7f6f6
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201212001.zip
+Update:   ESXi510-201210001
+Download: https://download2.vmware.com/patch/software/VUM/OFFLINE/release-364-20121022-316291/ESXi510-201210001.zip?HashKey=f9a8eb1fe79f6f379d4ef8015f037923&AuthKey=1401676093_695515bcc52410bd4da254ce667
+be1a3
+Missing:  /Users/spindler/Code/eclair/patches/ESXi510-201210001.zip
+```
+
+List the patches in the local repository:
+
+```
+$ eclair.rb -L
+ESXi550-201404001.zip
+ESXi550-201404020.zip
+```
+
+Example ~/.esxpasswd entry (same username and password for all hosts):
+
+```
+*:root:XXXX
+```
+
+Example ~/.esxpasswd entry (different password for different hosts):
+
+```
+host1:root:XXXX
+host2:root:YYYY
+```
+
---
+++ eclair.rb
@@ -0,0 +1,496 @@
+#!/usr/bin/env ruby
+
+# Name:         eclair (ESX Command Line Automation In Ruby)
+# Version:      0.0.8
+# Release:      1
+# License:      Open Source
+# Group:        System
+# Source:       N/A
+# URL:          http://lateralblast.com.au/
+# Distribution: ESX(i)
+# Vendor:       UNIX
+# Packager:     Richard Spindler <richard@lateralblast.com.au>
+# Description:  Ruby script to drive/setup ESX(i)
+
+require 'net/ssh'
+require 'net/scp'
+require 'etc'
+require 'expect'
+require 'getopt/std'
+require 'selenium-webdriver'
+require 'phantomjs'
+require 'nokogiri'
+
+# Set some defaults
+
+script    = $0
+options   = "AbCDf:hl:LP:r:Rs:Sp:u:UVyZ"
+username  = ""
+password  = ""
+mode      = "check"
+doaction  = ""
+filename  = ""
+patchdir  = Dir.pwd+"/patches"
+release   = "5.5.0"
+download  = "n"
+reboot    = "n"
+
+# VMware URLs
+
+product_url = "https://www.vmware.com/patchmgr/findPatch.portal"
+depot_url   = "http://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml"
+
+# Password file (can be used so username and password are not shown in command line)
+# ESX password file has the format "host:user:password"
+# If all systems have the same username and password then an entry of:
+# ALL:username:password or *:username:password will work
+
+esx_password_file = Etc.getpwuid.dir+"/.esxpasswd"
+
+# If file exists give it sensible permissions
+
+if File.exist?(esx_password_file)
+  %x[chmod 600 #{esx_password_file}]
+end
+
+
+# Print usage information
+
+def print_usage(script,options)
+  puts
+  puts "Usage: "+script+" -["+options+"]"
+  puts
+  puts "-h:\tPrint usage information"
+  puts "-V:\tPrint version information"
+  puts "-U:\tUpdate ESX if newer patch level is available"
+  puts "-Z:\tDowngrade ESX to earlier release"
+  puts "-L:\tList all available versions in local patch directory"
+  puts "-R:\tList all available versions in VMware depot"
+  puts "-A:\tDownload available patches to local patch directory"
+  puts "-C:\tCheck if newer patch level is available"
+  puts "-r:\tUpgrade or downgrade to a specific release"
+  puts "-s:\tHostname"
+  puts "-p:\tPassword"
+  puts "-f:\tSource file for update"
+  puts "-D:\tPatch directory (default is patches in sames directory as script)"
+  puts "-y:\tPerform action (if not given you will be prompted before upgrades)"
+  puts "-S:\tSetup ESXi (Syslog, NTP, etc)"
+  puts "-l:\tCheck if a particular patch is in the local repository"
+  puts "-b:\tPerform reboot after patch installation (not default)"
+  puts
+  return
+end
+
+# Get version information fro script header
+
+def get_version(script)
+  file_array = IO.readlines script
+  version    = file_array.grep(/^# Version/)[0].split(":")[1].gsub(/^\s+/,'').chomp
+  packager   = file_array.grep(/^# Packager/)[0].split(":")[1].gsub(/^\s+/,'').chomp
+  name       = file_array.grep(/^# Name/)[0].split(":")[1].gsub(/^\s+/,'').chomp
+  return version,packager,name
+end
+
+# Print script version information
+
+def print_version(script)
+  (version,packager,name) = get_version(script)
+  puts name+" v. "+version+" "+packager
+  exit
+end
+
+# Get command line arguments
+# Print help if given none
+
+if !ARGV[0]
+  print_usage(script,options)
+  exit
+end
+
+begin
+  opt = Getopt::Std.getopts(options)
+rescue
+  print_usage(script,options)
+  exit
+end
+
+# Print version
+
+if opt["V"]
+  print_version(script)
+  exit
+end
+
+# Prient usage
+
+if opt["h"]
+  print_usage(script,options)
+end
+
+# Set local patch director if given -P options
+
+if opt["P"]
+  patchdir = opt["P"]
+end
+
+# If given -U option set mode to upgrade
+
+if opt["U"]
+  mode = "up"
+end
+
+# If given -C option set mode to check/compare only
+
+if opt["C"]
+  mode = "check"
+end
+
+# If given -Z option set mode to downgrade
+
+if opt["Z"]
+  mode = "down"
+end
+
+# If given  -y option perform tasks without asking
+
+if opt["y"]
+  doaction = "y"
+end
+
+# If given -r option set te release number of ESX (e.g. 5.1.0)
+
+if opt["r"]
+  release = opt["r"]
+end
+
+# If given -b option reboot after patch installation
+
+if opt["b"]
+  reboot = "y"
+end
+
+# Routine to check a file exists
+# If just given a patch number it tries to determine if a file that matches
+# the patch number is available in the repository.
+
+def check_file(filename,patchdir)
+  if !filename.match(/\//)
+    patch_list = Dir.entries(patchdir)
+    filename   = patch_list.grep(/#{filename}/)[0].chomp
+    filename   = patchdir+"/"+filename
+  end
+  if !File.exist?(filename)
+    puts "File:  "+filename+" does not exist"
+    exit
+  else
+    puts "File:  "+filename
+  end
+  return filename
+end
+
+# Get the local version of update installed on and ES host
+
+def get_local_version(ssh_session,filename)
+  local_version = ssh_session.exec!("esxcli software profile get 2>&1 |head -1 |cut -f3 -d-").chomp
+  return ssh_session,local_version
+end
+
+# Get the local version of OS installed on and ES host
+
+def get_os_version(ssh_session)
+  os_version   = ssh_session.exec!("uname -r").chomp
+  return ssh_session,os_version
+end
+
+# Get the latest version of update available on the VMware site or in local repository
+
+def get_depot_version(ssh_session,filename,depot_url,os_version)
+  if !filename.match(/[A-z]/)
+    ssh_session.exec!("esxcli network firewall ruleset set -e true -r httpClient")
+    depot_version = ssh_session.exec!("esxcli software sources profile list -d #{depot_url} 2>&1 | grep '#{os_version}' |head -1 |awk '{print $1}'").chomp
+  else
+    tmp_dir = "/tmp/esxzip"
+    if !File.directory?(tmp_dir)
+      Dir.mkdir(tmp_dir)
+    end
+    %x[unzip -o #{filename} metadata.zip -d #{tmp_dir}]
+    depot_version = %x[unzip -l #{tmp_dir}/metadata.zip |awk '{print $4}' |grep '^profiles' |grep standard].chomp
+    depot_version = depot_version.split(/\//)[1].split("-")[0..-2].join("-")
+  end
+  return ssh_session,depot_version
+end
+
+# Compare the local version of update installed on the ESX host with what is
+# available on the VMware site on in the local repository
+
+def compare_versions(local_version,depot_version,mode)
+  if local_version.match(/-/)
+    local_version = local_version.gsub(/-standard/,"")
+  end
+  if depot_version.match(/-/)
+    depot_version = depot_version.split(/-/)[2]
+  end
+  puts "Current:   "+local_version
+  puts "Available: "+depot_version
+  if mode =~ /up|check/
+    if depot_version > local_version
+      puts "Depot patch level is newer than installed version"
+      update_available = "y"
+    else
+      update_available = "n"
+      puts "Local patch level is up to date"
+    end
+  else
+    if depot_version < local_version
+      puts "Depot patch level is lower than installed version"
+      update_available = "y"
+    else
+      update_available = "n"
+      puts "Local patch level is up to date"
+    end
+  end
+  if mode == "check"
+    exit
+  end
+  return update_available
+end
+
+# Actual routine to Update/Downgrade ESX host software
+
+def update_software(ssh_session,hostname,username,password,local_version,depot_version,filename,mode,doaction,depot_url,reboot)
+  update_available = compare_versions(local_version,depot_version,mode)
+  if update_available == "y" and mode != "check"
+    if filename.match(/[A-z]/)
+      patch_file = File.basename(filename)
+      depot_dir  = "/scratch/downloads"
+      depot_file = depot_dir+"/"+patch_file
+      ssh_session.exec!("mkdir #{depot_dir}")
+      puts "Copying local file "+filename+" to "+hostname+":"+depot_file
+      Net::SCP.upload!(hostname, username, filename, depot_file, :ssh => { :password => password })
+      depot_version = ssh_session.exec!("esxcli software sources profile list -d=#{depot_file} |grep standard |awk '{print $1}'").chomp
+    end
+    if doaction != "y"
+      while doaction !~ /y|n/
+        print "Install update [y,n]: "
+        doaction = gets.chomp
+      end
+    end
+    if doaction == "y"
+      if filename.match(/[A-z]/)
+        puts "Installing "+depot_version+" from "+depot_file
+        output = ssh_session.exec!("esxcli software profile install -d=#{depot_file} -p=#{depot_version}")
+      else
+        puts "Installing "+depot_version+" from "+depo_url
+        ssh_session.exec!("esxcli network firewall ruleset set -e true -r httpClient")
+        output = ssh_session.exec!("esxcli software profile update -d=#{depot_url} -p=#{depot_version}")
+      end
+    else
+      puts "Not installing patch "+local_version
+      exit
+    end
+    puts output
+    if output.match(/Reboot Required: true/) and reboot == "y"
+      puts "Rebooting"
+      ssh_session.exec!("reboot")
+    end
+  end
+  return ssh_session
+end
+
+# Main routing called to Update/Downgrade ESX software
+
+def update_esxi(hostname,username,password,filename,mode,doaction,depot_url,reboot)
+  begin
+    Net::SSH.start(hostname, username, :password => password, :paranoid => false) do |ssh_session|
+      (ssh_session,os_version)    = get_os_version(ssh_session)
+      (ssh_session,local_version) = get_local_version(ssh_session,filename)
+      (ssh_session,depot_version) = get_depot_version(ssh_session,filename,depot_url,os_version)
+      ssh_session = update_software(ssh_session,hostname,username,password,local_version,depot_version,filename,mode,doaction,depot_url,reboot)
+    end
+  rescue Net::SSH::HostKeyMismatch => host
+    puts "Existing key found for "+hostname
+    if doaction != "y"
+      while doaction !~ /y|n/
+        print "Update host key [y,n]: "
+        doaction = gets.chomp
+      end
+    end
+    if doaction == "y"
+      puts "Updating host key for "+hostname
+      host.remember_host!
+    else
+      exit
+    end
+    retry
+  end
+  return
+end
+
+# Get a list of patches in the local repository
+
+def list_local_patches(patchdir)
+  if File.directory?(patchdir)
+    file_list = Dir.entries(patchdir)
+    file_list.each do |local_file|
+      if local_file.match(/zip$/)
+        puts local_file
+      end
+    end
+  end
+end
+
+# Get a list of the available patches on the vmware site and put them into a hash
+# Need to send dropdown box seletions twice to ensure they stick
+# Need to search for button to click via src tag due to malformed HTML
+
+def get_vmware_patch_info(product_url,username,password,release)
+  update_list = {}
+  update = ""
+  driver = Selenium::WebDriver.for :phantomjs
+  driver.get(product_url)
+  driver.find_element(:name => "product").find_element(:css,"option[value='ESXi (Embedded and Installable)']").click
+  driver.find_element(:name => "product").find_element(:css,"option[value='ESXi (Embedded and Installable)']").click
+  driver.find_element(:name => "version").send_keys(release)
+  driver.find_element(:name => "version").send_keys(release)
+  driver.find_element(:xpath,"//*[@src='/patchmgr/resources/images/support_search_button.gif']").click
+  page = driver.page_source
+  page = Nokogiri::HTML.parse(page)
+  info = page.css("span").css("input")
+  info.each do |download|
+    value = download["value"]
+    if !value.match(/http/)
+      update = value
+    else
+      if value.match(/ESX/)
+        update_list[update] = value
+      end
+    end
+  end
+  return update_list
+end
+
+# Process the hash of patches returned from the VMware site and check they are
+# in the local repository, if the download option is given download the patches
+# if they are not in the local repository
+
+def process_vmware_patch_info(patch_list,download,patchdir)
+  patch_list.each do |patch_no, patch_url|
+    puts "Update:   "+patch_no
+    puts "Download: "+patch_url
+    local_file = patch_url.split(/\?/)[0]
+    local_file = File.basename(local_file)
+    local_file = patchdir+"/"+local_file
+    if File.exist?(local_file)
+      puts "Present:  "+local_file
+    else
+      puts "Missing:  "+local_file
+    end
+    if download == "y"
+      %x[wget "#{patch_url}" -O "#{local_file}"]
+    end
+  end
+  return
+end
+
+# If given -L option list patches in local repository
+
+if opt["L"]
+  list_local_patches(patchdir)
+  exit
+end
+
+# If the password isn't given on the command line, try to retrieve it from
+# the .esxpasswd file. Otherwise ask for it.
+
+def get_password(esx_password_file,hostname)
+  if File.exist?(esx_password_file)
+    all_check = %x[cat #{esx_password_file} |egrep "^\\*:|^ALL:"]
+    if all_check.match(/\*|ALL/)
+      password = %x[cat #{esx_password_file} |cut -f3 -d:].chomp
+    else
+      password = %x[cat #{esx_password_file} |grep '^#{hostname}' |cut -f3 -d:].chomp
+    end
+  else
+    while password !~/[A-z]|[0-9]/ do
+      print "Password: "
+      gets password
+    end
+  end
+  return password
+end
+
+# If the username isn't given on the command line, try to retrieve it from
+# the .esxpasswd file. Otherwise ask for it.
+
+def get_username(esx_password_file,hostname)
+  if File.exist?(esx_password_file)
+    all_check = %x[cat #{esx_password_file} |egrep "^\\*:|^ALL:"]
+    if all_check.match(/\*|ALL/)
+      username = %x[cat #{esx_password_file} |cut -f2 -d:].chomp
+    else
+      username = %x[cat #{esx_password_file} |grep '^#{hostname}' |cut -f2 -d:].chomp
+    end
+  else
+    while username !~ /[A-z]/
+      print "Username: "
+      gets username
+    end
+  end
+  return username
+end
+
+# Check if a particular patch is in the local repository
+
+if opt["l"]
+  patch_no = opt["l"]
+  puts "Patch: "+patch_no
+  filename = check_file(patch_no,patchdir)
+  exit
+end
+
+# If given the -A option download patches that are not in the local repository
+
+if opt["A"]
+  download = "y"
+end
+
+# If given the -A or -R option check the available patches on the VMware site
+# Also checks if they are present in the local repository
+
+if opt["R"] or opt["A"]
+  patch_list = get_vmware_patch_info(product_url,username,password,release)
+  process_vmware_patch_info(patch_list,download,patchdir)
+  exit
+end
+
+if opt["U"] or opt["C"] or opt["D"]
+  if !opt["s"]
+    puts "No server name given"
+    exit
+  else
+    hostname = opt["s"]
+  end
+  if password !~ /[A-z]/
+    if !opt["p"]
+      password = get_password(esx_password_file,hostname)
+    else
+      password = opt["p"]
+    end
+  end
+  if username !~ /[A-z]/
+    if !opt["u"]
+      username = get_username(esx_password_file,hostname)
+    else
+      username = opt["u"]
+    end
+  end
+  if opt["f"]
+    filename = opt["f"]
+    filename = check_file(filename,patchdir)
+  end
+  hostname = opt["s"]
+  if opt["U"] or opt["C"] or opt["Z"]
+    update_esxi(hostname,username,password,filename,mode,doaction,depot_url,reboot)
+  end
+  exit
+end
